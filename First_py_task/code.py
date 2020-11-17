c2a67b25828add4995f4f40f2b3154363a7165b2
Aravindhan <aravindhan405@gmail.com>
2020-11-17 12:40:08 +0530

    Code file created.

---
+++ First_py_task/code.py
@@ -0,0 +1,66 @@
+# --------------
+# Code starts here
+
+# Create the lists 
+class_1 = ["Geoffrey Hinton","Andrew Ng","Yoshua Beingo"]
+class_2 = ["Hilary Mason","Carla Gentry","Corinna Cortes"]
+# Concatenate both the strings
+new_class = class_1 + class_2
+print(new_class)
+# Append the list
+new_class.append("Peter Warden")
+# Print updated list
+print(new_class)
+
+# Remove the element from the list
+new_class.remove("Carla Gentry")
+# Print the list
+print(new_class)
+
+
+# Create the Dictionary
+courses = {"Math" : 65,"English" : 70,"History" : 80,"French" : 70,"Science" : 60}
+
+
+# Slice the dict and stores  the all subjects marks in variable
+M = courses["Math"]
+E = courses["English"]
+H = courses["History"]
+F = courses["French"] 
+S = courses["Science"]
+# Store the all the subject in one variable `Total`
+total = courses["Math"] + courses["English"] + courses["History"] + courses["French"] + courses["Science"] 
+# Print the total
+print(total)
+# Insert percentage formula
+percentage = total / 500 * 100
+# Print the percentage
+print(percentage)
+
+
+
+# Create the Dictionary
+mathematics = {"Geoffrey Hinton" : 78, "Andrew Ng" : 95, "Sebastian Raschka" : 65, "Yoshua Benjio" : 50,"Hilary Mason" : 70,"Corinna Cortes" : 66,"Peter Warden" : 75 }
+
+topper = max(mathematics,key=mathematics.get)
+print(topper)
+
+# Given string
+split_name=topper.split(" ")
+first_name = split_name[0]
+last_name = split_name[1]
+
+# Create variable first_name 
+
+# Create variable Last_name and store last two element in the list
+
+# Concatenate the string
+full_name = last_name + " " + first_name
+# print the full_name
+print(full_name)
+# print the name in upper case
+certificate_name = full_name.upper()
+print(certificate_name)
+# Code ends here
+
+
